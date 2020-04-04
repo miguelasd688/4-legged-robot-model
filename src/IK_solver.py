c08fd3baa2ccc52a6ec645474d593787f34142db
miguelasd688 <35175691+miguelasd688@users.noreply.github.com>
2020-04-04 18:06:48 +0200

    IK equations written on pybullet frame

--- src/IK_solver.py
+++ src/IK_solver.py
@@ -31,36 +31,21 @@ def checkdomain(D):
 "    | /                 "
 "    |/____________  x       "
 """
-def solve_R(coord, coxa, femur, tibia):
-    x = coord[1]  #it changes the body_frame to the leg_frame
-    y = -coord[2] #(which has different axis)
-    z = -coord[0]
-    
-    """LEG DIMENTIONS"""
-    # coxa = 0.036
-    # femur = 0.11
-    # tibia = 0.11
-    D = (x**2+y**2-coxa**2+z**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
+#IK equations now written in pybullet frame.
+def solve_R(coord , coxa , femur , tibia): 
+    D = (coord[1]**2+(-coord[2])**2-coxa**2+(-coord[0])**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
     D = checkdomain(D)
     gamma = numpy.arctan2(-numpy.sqrt(1-D**2),D)
-    tetta = -numpy.arctan2(-y,x)-numpy.arctan2(numpy.sqrt(x**2+y**2-coxa**2),-coxa)
-    alpha = numpy.arctan2(z,numpy.sqrt(x**2+y**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
-    angles = numpy.matrix([-tetta, alpha, gamma])
+    tetta = -numpy.arctan2(coord[2],coord[1])-numpy.arctan2(numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2),-coxa)
+    alpha = numpy.arctan2(-coord[0],numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
+    angles = numpy.array([-tetta, alpha, gamma])
     return angles
 
-def solve_L(coord, coxa, femur, tibia):
-    x = coord[1]  #it changes the body_frame to the leg_frame
-    y = -coord[2] #(which has different axis)
-    z = -coord[0]
-    
-    """LEG DIMENTIONS"""
-    # coxa = -0.036
-    # femur = 0.11
-    # tibia = 0.11
-    D = (x**2+y**2-coxa**2+z**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
+def solve_L(coord , coxa , femur , tibia):
+    D = (coord[1]**2+(-coord[2])**2-coxa**2+(-coord[0])**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
     D = checkdomain(D)
     gamma = numpy.arctan2(-numpy.sqrt(1-D**2),D)
-    tetta = -numpy.arctan2(-y,x)-numpy.arctan2(numpy.sqrt(x**2+y**2-coxa**2),-coxa)
-    alpha = numpy.arctan2(z,numpy.sqrt(x**2+y**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
-    angles = numpy.matrix([-tetta, alpha, gamma])
+    tetta = -numpy.arctan2(coord[2],coord[1])-numpy.arctan2(numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2),coxa)
+    alpha = numpy.arctan2(-coord[0],numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
+    angles = numpy.array([-tetta, alpha, gamma])
     return angles
