af20f3fb5de788d50379b5a29326016e06145baa
miguelasd688 <35175691+miguelasd688@users.noreply.github.com>
2020-04-17 11:07:37 +0200

    Update pybullet_debuger.py

--- src/pybullet_debuger.py
+++ src/pybullet_debuger.py
@@ -18,15 +18,36 @@ class pybulletDebug:
         self.cdist=0.66
         time.sleep(0.5)
         
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 22:15:21 2020
+
+@author: linux-asd
+"""
+import pybullet as p
+import time
+import numpy as np
+import sys
+
+class pybulletDebug:
+    def __init__(self):
+        #Camera paramers to be able to yaw pitch and zoom the camera (Focus remains on the robot) 
+        self.cyaw=90
+        self.cpitch=-7
+        self.cdist=0.66
+        time.sleep(0.5)
+        
         self.xId = p.addUserDebugParameter("x" , -0.10 , 0.10 , 0.)
         self.yId = p.addUserDebugParameter("y" , -0.10 , 0.10 , 0.)
         self.zId = p.addUserDebugParameter("z" , -0.10 , 0.10 , 0.)
         self.rollId = p.addUserDebugParameter("roll" , -np.pi/4 , np.pi/4 , 0.)
         self.pitchId = p.addUserDebugParameter("pitch" , -np.pi/4 , np.pi/4 , 0.)
         self.yawId = p.addUserDebugParameter("yaw" , -np.pi/4 , np.pi/4 , 0.)
-        self.LId = p.addUserDebugParameter("L" , 0. , 1.5 , 0.)
+        self.LId = p.addUserDebugParameter("L" , -0.5 , 1.5 , 0.)
+        self.LrotId = p.addUserDebugParameter("Lrot" , -1.5 , 1.5 , 0.)
         self.angleId = p.addUserDebugParameter("angleWalk" , -180. , 180. , 0.)
-        self.periodId = p.addUserDebugParameter("stepPeriod" , 0.1 , 2. , 1.)
+        self.periodId = p.addUserDebugParameter("stepPeriod" , 0.1 , 3. , 2.5)
     
     def cam_and_robotstates(self , boxId):
                 ####orientacion de la camara
@@ -53,7 +74,8 @@ class pybulletDebug:
         pos = np.array([p.readUserDebugParameter(self.xId),p.readUserDebugParameter(self.yId), p.readUserDebugParameter(self.zId)])
         orn = np.array([p.readUserDebugParameter(self.rollId),p.readUserDebugParameter(self.pitchId), p.readUserDebugParameter(self.yawId)])
         L = p.readUserDebugParameter(self.LId)
+        Lrot = p.readUserDebugParameter(self.LrotId)
         angle = p.readUserDebugParameter(self.angleId)
         T = p.readUserDebugParameter(self.periodId)
         
-        return pos , orn , L , angle , T
+        return pos , orn , L , angle , Lrot , T
